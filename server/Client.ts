9eb7651bd8afedd828895ca1d997346f60fbc5b2
Steven <ssebexen@gmail.com>
2024-07-30 15:23:31 -0400

    Server implemented

--- server/Client.ts
+++ server/Client.ts
@@ -1,10 +1,43 @@
 import WS from 'ws';
+import { ClientInfo } from './ClientInfo';
+import { Message, MessageIdentity, MessageType } from '../common/Message';
 
-export type ClientInfo = {
-  user: string;
-};
-
-export type Client = {
-  info: ClientInfo;
+export class Client {
   socket: WS;
-};
+  info: ClientInfo | null = null;
+  onClack: (() => void) | null = null;
+  onClose: ((clientToClose: Client) => void) | null = null;
+
+  constructor(socket: WS) {
+    this.socket = socket;
+    this.socket.on('message', this.onMessage.bind(this));
+    this.socket.on('close', this._onClose.bind(this));
+  }
+
+  onMessage(data: string) {
+    console.log(`Client '${this.info?.user}' sent message: ${data}`);
+    const message: Message = JSON.parse(data);
+    switch(message.type) {
+      case MessageType.Identity:
+        this.establishIdentity((message as MessageIdentity).user);
+        break;
+      case MessageType.Clack:
+        this.clack();
+        break;
+    }
+  }
+
+  _onClose() {
+    if (!this.onClose) return;
+    this.onClose(this);
+  }
+
+  establishIdentity(user: string) {
+    this.info = new ClientInfo(user);
+  }
+
+  clack() {
+    if (!this.onClack) return;
+    this.onClack();
+  }
+}
--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,52 @@
-import Express from 'express';
+import WS from 'ws';
+import { Client } from './Client';
+import { Message, MessageClack, MessageCount, MessageType } from '../common/Message';
+import { ServerState } from './ServerState';
+
+const DEFAULT_PORT = 8080;
+
+export class Server {
+  socket: WS.Server;
+  connectedClients: Client[] = [];
+  serverState: ServerState = new ServerState();
+
+  constructor(port?: number) {
+    this.socket = new WS.WebSocketServer({ port: port ?? DEFAULT_PORT });
+    this.socket.on('connection', this.onConnection.bind(this));
+  }
+
+  _sendMessage(message: Message, client: Client) {
+    client.socket.send(JSON.stringify(message));
+  }
+  
+  sendCount(client: Client) {
+    const message = new MessageCount(this.serverState.clackCount);
+    this._sendMessage(message, client);
+  }
+  
+  broadcastCount() {
+    this.connectedClients.forEach(client => {
+      this.sendCount(client);
+    });
+  }
+  
+  clack() {
+    this.serverState.clackCount += 1;
+    this.broadcastCount();
+  }
+
+  onClientClose(clientToClose: Client) {
+    const toCloseIndex = this.connectedClients.findIndex(client => client === clientToClose);
+    if (toCloseIndex === -1) throw new Error('Unable to find client to close.');
+    this.connectedClients.splice(toCloseIndex, 1);
+  }
+  
+  onConnection(socket: WS) {
+    console.log('Client connected');
+    const client = new Client(socket);
+    client.onClack = this.clack.bind(this);
+    client.onClose = this.onClientClose.bind(this);
+    this.sendCount(client);
+    this.connectedClients.push(client);
+  }
+}
